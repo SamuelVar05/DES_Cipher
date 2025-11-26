a3f422e708f0abacb1b4f0b112bb1498f9186fe3
Samuel Vargas <sjvc0595@gmail.com>
2025-11-26 15:24:35 -0500

    feat: Improve encryption and decryption routes with error handling and password input; update index.html for secure key entry

--- routes/file_routes.py
+++ routes/file_routes.py
@@ -1,16 +1,34 @@
-from flask import Blueprint, request, send_file
+from flask import Blueprint, request, send_file, jsonify
 from controllers.file_controller import handle_encrypt, handle_decrypt
 
 file_routes = Blueprint("file_routes", __name__)
 
 @file_routes.route("/encrypt", methods=["POST"])
 def encrypt_route():
+    if "file" not in request.files:
+        return jsonify({"error": "No se envió ningún archivo"}), 400
+
     file = request.files["file"]
-    output_path = handle_encrypt(file)
+    user_key = request.form.get("key")
+
+    if not user_key:
+        return jsonify({"error": "Debe enviar la clave"}), 400
+
+    output_path = handle_encrypt(file, user_key)
     return send_file(output_path, as_attachment=True)
+
 
 @file_routes.route("/decrypt", methods=["POST"])
 def decrypt_route():
+    if "file" not in request.files:
+        return jsonify({"error": "No se envió ningún archivo"}), 400
+
     file = request.files["file"]
-    output_path = handle_decrypt(file)
+    user_key = request.form.get("key")
+
+    if not user_key:
+        return jsonify({"error": "Debe enviar la clave"}), 400
+
+    output_path = handle_decrypt(file, user_key)
     return send_file(output_path, as_attachment=True)
+
