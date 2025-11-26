00a6898202ce93d26379b153c164c9845d63474b
Samuel Vargas <sjvc0595@gmail.com>
2025-11-26 15:21:03 -0500

    fix: Update BASE_DIR calculation in settings.py and add uploads directory to .gitignore

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,7 @@
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 UPLOAD_DIR = os.path.join(BASE_DIR, "uploads")
 ENCRYPT_DIR = os.path.join(UPLOAD_DIR, "encrypted")
 DECRYPT_DIR = os.path.join(UPLOAD_DIR, "decrypted")
